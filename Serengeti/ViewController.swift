217c83f0aa2360d589089ca9e69e166e7760d4b6
Suyeol Jeon <devxoul@gmail.com>
2015-12-25 03:33:06 +0900

    ViewController에 addressInput 속성 추가

--- Serengeti/ViewController.swift
+++ Serengeti/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var webView: UIWebView!
     @IBOutlet var toolbar: UIToolbar!
+    @IBOutlet var addressInput: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
